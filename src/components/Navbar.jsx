a0ccb6af627595abd90a8e96906957fcf01f170f
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2022-09-24 18:02:00 +0000

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
             <li className="hover:underline underline-offset-[1.6rem] cursor-pointer decoration-4 decoration-green-600">Careers</li>
         </ul>
      </div>
-     <button className="btn bg-gradient-to-r from-green-400 to-blue-500 border border-blue-800 rounded-3xl outline-none text-white py-2 px-5 hover:opacity-[0.7]"type="button">Request Invite</button>
+     <button className="btn bg-gradient-to-r from-green-400 to-blue-500 border border-blue-800 rounded-3xl outline-none text-white py-2 px-5 hover:opacity-[0.7]" type="button">Request Invite</button>
     </nav>
   );
 };
