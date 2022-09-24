7887f60997330989c0d517c850678628e9b0172c
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2022-09-24 17:42:06 +0000

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,20 @@ import logo from '../assets/logo.svg';
 
 const Navbar = () => {
   return (
-    <nav className="navbar flex justify-between">
+    <nav className="navbar flex justify-between py-4 px-10 border shadow-lg shadow-indigo-500/10 box-border">
      <picture>
         <img src={logo} alt=""/>
      </picture>
-     <div className="justify-self-center">
-        <ul className="flex ">
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
+     <div className="justify-self-center text-lg ">
+        <ul className="flex gap-x-20  ">
+            <li className="hover:underline underline-offset-[2.14rem] cursor-pointer decoration-4 decoration-green-600" >Home</li>
+            <li className="hover:underline underline-offset-[2.14rem] cursor-pointer decoration-4 decoration-green-600">About</li>
+            <li className="hover:underline underline-offset-[2.14rem] cursor-pointer decoration-4 decoration-green-600">Contact</li>
+            <li className="hover:underline underline-offset-[2.14rem] cursor-pointer decoration-4 decoration-green-600">Blog</li>
+            <li className="hover:underline underline-offset-[2.14rem] cursor-pointer decoration-4 decoration-green-600">Careers</li>
         </ul>
      </div>
-
-     <button type="button">Send a message</button>
+     <button className="btn bg-gradient-to-r from-green-400 to-blue-500 border border-blue-800 rounded-3xl outline-none text-white py-2 px-5"type="button">Request Invite</button>
     </nav>
   );
 };
