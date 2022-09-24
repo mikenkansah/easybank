28932ea11d0443676b67ac4c3ea4cffc0b598a17
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2022-09-24 16:45:58 +0000

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
+import logo from '../assets/logo.svg';
+
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar flex justify-between">
      <picture>
-        {/* <img src={} alt=""/> */}
+        <img src={logo} alt=""/>
      </picture>
+     <div className="justify-self-center">
+        <ul className="flex ">
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+     </div>
+
+     <button type="button">Send a message</button>
     </nav>
   );
 };
