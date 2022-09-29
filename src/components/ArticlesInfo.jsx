13c72d56d174d2827935a1d69d1e577eb76da263
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2022-09-29 13:27:13 +0000

    updates

--- src/components/ArticlesInfo.jsx
+++ src/components/ArticlesInfo.jsx
@@ -3,8 +3,8 @@ const Articles = ({ img, body, heading, small }) => {
     <article className="bg-slate-100">
       <img src={img} alt="" />
       <div className="px-6 py-4">
-        <small>{small}</small>
-        <h4>{heading}</h4>
+        <small className=" text-muted">{small}</small>
+        <h4 className="text-2xl hover:text-green-400 cursor-pointer">{heading}</h4>
         <p>{body}</p>
       </div>
     </article>
