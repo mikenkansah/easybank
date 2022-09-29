a636d28fc6c65097b9018d51e1e2da4c67e16ca7
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2022-09-29 15:37:03 +0000

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,12 @@ import instagram from "../assets/icon-instagram.svg";
 import twitter from "../assets/icon-twitter.svg";
 import youtube from "../assets/icon-youtube.svg";
 import pinterest from "../assets/icon-pinterest.svg";
+import Button from './buttons/Button';
 
 const Footer = () => {
   return (
     <footer className="px-10 py-10 bg-cyan-800">
-      <div className="flex flex-col md:flex-row ">
+      <div className="flex flex-col md:flex-row md:justify-between align-center ">
         <div>
           <img alt="" src={logo} />
           <div className="flex flex-row justify-between">
@@ -29,7 +30,13 @@ const Footer = () => {
           <h4>Support</h4>
           <h4>Privacy Policy</h4>
         </div>
+        <div className="text-white">
+         <Button/>
+      
+          <h4>&copy; EasyBank All rights reserved</h4>
+        </div>
       </div>
+    
     </footer>
   );
 };
