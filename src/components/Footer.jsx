5ead1517bf6bc63c6b60a04cea8279f679bba50c
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2022-09-29 13:50:32 +0000

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
+import logo from '../assets/logo.svg';
+import facebook from '../assets/icon-facebook.svg';
+import instagram from '../assets/icon-instagram.svg';
+
+
 const Footer = () => {
-    return (  
-        <footer>
-            
-        </footer>
-    );
-}
- 
+  return (
+    <footer className="px-10 py-10 bg-cyan-800">
+      <div className="flex flex-col md:flex-row ">
+        <div>
+            <img alt=""  src={logo}/>
+            <div className="flex flex-row justify-between" >
+                <img alt="" src={facebook}/>
+                <img alt="" src={instagram}/>
+                <img alt="" src={facebook}/>
+                <img alt="" src={facebook}/>
+            </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
