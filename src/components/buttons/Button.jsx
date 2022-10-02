6e404fdcb3fd56319583e2d7fdd3373d5b19463f
Michael Kwame Nkansah <nkansahmichael232@gmail.com>
2022-10-02 20:43:02 +0000

    updates

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,6 +1,6 @@
-const Button = () => {
+const Button = ({name}) => {
     return ( 
-        <button className="btn bg-gradient-to-r from-green-400 to-blue-500 border border-blue-800 rounded-3xl outline-none text-white py-2 px-5 hover:opacity-[0.7]" type="button">Request Invite</button>
+        <button className="btn bg-gradient-to-r from-green-400 to-blue-500 border border-blue-800 rounded-3xl outline-none text-white py-2 px-5 hover:opacity-[0.7]" type="button">{name}</button>
      );
 }
 
